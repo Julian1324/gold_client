dee5afb0a9a5f01c4d04b3e0d0f9255197d85dd6
Julian1324 <julian-ortiz98@hotmail.com>
2024-04-10 20:41:15 -0500

    Detailing card

--- src/shared/Modal/productDetailModal.jsx
+++ src/shared/Modal/productDetailModal.jsx
@@ -9,14 +9,17 @@ const ProductDetailModal = ({ show, onHide, name, image, body, price }) => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    <h4>{name}</h4>
+                    <h4>Detalle</h4>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <div className='d-flex '>
                     <img src={image} alt="" style={{ height: '30vh' }} />
-                    <div className='d-flex w-100 align-items-center p-4'>
-                        {body} ${price}
+                    <div className='d-flex flex-column p-4'>
+                        <div className='w-100'>{name}</div>
+                        <div className='d-flex w-100 align-items-center'>
+                            {body} ${price}
+                        </div>
                     </div>
                 </div>
             </Modal.Body>
