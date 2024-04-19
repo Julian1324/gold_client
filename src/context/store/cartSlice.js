b9456337c4f6c32db987989b22853154b142ab28
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-04-19 16:57:57 -0500

    AddtoCart. Optimization pending

--- src/context/store/cartSlice.js
+++ src/context/store/cartSlice.js
@@ -1,15 +1,18 @@
 export const cartSlice = (set, get) => ({
     items: [],
     addItem: (newItem) => set((state) => {
-        const updatedItems = [...state.items];
-        const arrayFiltered = updatedItems.filter((item) => item.name === newItem.name);
-        console.log('arrayFiltered', arrayFiltered);
-        if (!arrayFiltered.length) return { ...state, items: [...state.items, newItem] };
-        const objectUnified = arrayFiltered.reduce((acc, item) => {
-            acc = { ...item, quantityToBuy: (!!acc.quantityToBuy && acc.quantityToBuy) + newItem.quantityToBuy };
+        const stateItems = [...state.items];
+        const objFinded = stateItems.find((item) => item.name === newItem.name);
+        if (!stateItems.length || !objFinded) return { ...state, items: [...state.items, newItem] };
+        const objUnified = stateItems.reduce((acc, item) => {
+            if (item.name === newItem.name) Object.assign(acc, { ...item, quantityToBuy: item.quantityToBuy + newItem.quantityToBuy });
             return acc;
         }, {});
-        return { ...state, items: [...state.items, objectUnified] }
+        const updatedItems = stateItems.map((item) => {
+            if (item.name === objUnified.name) return { ...objUnified };
+            return item;
+        });
+        return { ...state, items: [...updatedItems] }
     }),
     setItems: (updatedItems) => set((state) => ({ ...state, items: [...updatedItems] })),
     getSubtotal: () => get().items.reduce((acc, item) => {
