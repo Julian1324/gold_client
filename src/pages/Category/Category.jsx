d6848c2e2ee5d5c238eba5d849010a35201e8e55
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-05-08 15:26:04 -0500

    InfiniteScroll in Category

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -3,22 +3,58 @@ import CardProduct from "../../components/Cards/CardProduct";
 import { useParams } from 'react-router-dom';
 import { getProductsByCategory } from "../../helpers/axiosHelper";
 import { getCategorySlice } from "../../context/store/store";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const Category = () => {
     const [products, setProducts] = useState([]);
     const { category_id } = useParams();
+    const [paginator, setPaginator] = useState({});
     const { getCategoryImageByID } = getCategorySlice();
 
     useEffect(() => {
         const getProducts = async () => {
             const response = await getProductsByCategory({ category_id, page: 1 });
-            console.log(response);
             setProducts(response.data.docs.map((product) => ({ ...product, ...getCategoryImageByID(category_id) })));
+            delete response.data.docs;
+            setPaginator(response.data);
         }
         getProducts();
     }, [category_id, getCategoryImageByID]);
+
+    const onScrollProducts = async (nextPage) => {
+        const response = await getProductsByCategory({ category_id, page: nextPage });
+        const newProducts = response.data.docs.map((product) => ({ ...product, ...getCategoryImageByID(product.category_id) }));
+        setProducts([...products, ...newProducts]);
+        delete response.data.docs;
+        setPaginator(response.data);
+    }
+
     return (
-        <div className="d-flex p-5 flex-wrap justify-content-center">
+        // <div className="d-flex p-5 flex-wrap justify-content-center">
+        //     {products.map((product, productIndex) => {
+        //         return (
+        //             <CardProduct
+        //                 _id={product._id}
+        //                 name={product.name}
+        //                 image={product.image}
+        //                 price={product.price}
+        //                 key={productIndex}
+        //                 body={product.description}
+        //                 discount={product.discount}
+        //                 quantity={product.quantity}
+        //                 status={product.status}
+        //             />
+        //         )
+        //     })}
+        //     {!products.length && <div style={{height: '50vh'}}>Por el momento no hay nada en esta categoría...</div>}
+        // </div>
+        <InfiniteScroll
+            dataLength={products.length}
+            next={() => onScrollProducts(paginator.nextPage)}
+            hasMore={paginator.hasNextPage}
+            loader={<div>Loading......................................</div>}
+            className="d-flex p-5 flex-wrap justify-content-center"
+        >
             {products.map((product, productIndex) => {
                 return (
                     <CardProduct
@@ -34,8 +70,7 @@ const Category = () => {
                     />
                 )
             })}
-            {!products.length && <div style={{height: '50vh'}}>Por el momento no hay nada en esta categoría...</div>}
-        </div>
+        </InfiniteScroll>
     )
 }
 
