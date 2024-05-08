3faf8b695533aa93a7bb4cbb5052b6969597c7c7
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-05-08 15:26:41 -0500

    Deleting comments

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -30,24 +30,6 @@ const Category = () => {
     }
 
     return (
-        // <div className="d-flex p-5 flex-wrap justify-content-center">
-        //     {products.map((product, productIndex) => {
-        //         return (
-        //             <CardProduct
-        //                 _id={product._id}
-        //                 name={product.name}
-        //                 image={product.image}
-        //                 price={product.price}
-        //                 key={productIndex}
-        //                 body={product.description}
-        //                 discount={product.discount}
-        //                 quantity={product.quantity}
-        //                 status={product.status}
-        //             />
-        //         )
-        //     })}
-        //     {!products.length && <div style={{height: '50vh'}}>Por el momento no hay nada en esta categoría...</div>}
-        // </div>
         <InfiniteScroll
             dataLength={products.length}
             next={() => onScrollProducts(paginator.nextPage)}
