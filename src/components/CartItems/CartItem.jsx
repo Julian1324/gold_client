a4644396e0abeed66834b94f35f463c4141e0384
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-05-10 17:26:24 -0500

    Pending last item added to cart (correct name), and currentQuantity in cart page

--- src/components/CartItems/CartItem.jsx
+++ src/components/CartItems/CartItem.jsx
@@ -1,7 +1,46 @@
-const CartItem = () => {
+import Form from 'react-bootstrap/Form';
+import { currencyValue } from '../../helpers/currencyHelper';
+import { constants } from '../../context/constants';
+
+const CartItem = ({ _id, name, image, currentQuantity, price, discount, quantityToBuy }) => {
+
+    const calculateDiscount = (thePrice, theDiscount) => {
+        return thePrice - (thePrice * theDiscount / 100);
+    }
+
     return (
         <>
-            CartItem ----=
+            <div className="d-flex align-items-center mt-2 me-4 bg-light rounded" style={{ height: '15vh' }}>
+                <Form.Check type={'checkbox'} id={'checkbox'} className='ms-2' />
+                <img src={image} className="rounded ms-2" alt="" style={{ width: '5vw' }} />
+                <div className='d-flex flex-column align-items-top ms-2' style={{ height: '5vw', width: '24vw' }}>
+                    <h5>{name}</h5>
+                    <span className='d-flex mb-2 h-100 align-items-end'>
+                        Disponibilidad:
+                        {!currentQuantity ?
+                            <span className='text-danger ms-1'> Agotado</span>
+                            :
+                            <span className='text-primary ms-1'> {currentQuantity} unidades </span>
+                        }
+                    </span>
+                </div>
+                <div>
+                    {!discount ?
+                        <div className='text-success'>
+                            {currencyValue(price)} {constants.CURRENCY_NAME}
+                        </div>
+                        :
+                        <div className='d-flex flex-column'>
+                            <div className='text-success'>
+                                {currencyValue(calculateDiscount(price, discount))} {constants.CURRENCY_NAME}
+                            </div>
+                            <div className='text-decoration-line-through text-secondary'>
+                                {currencyValue(price)} {constants.CURRENCY_NAME}
+                            </div>
+                        </div>
+                    }
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,19 +1,39 @@
 import { getCartSlice } from '../../context/store/store';
 import CartItem from '../../components/CartItems/CartItem';
+import { useEffect } from 'react';
 
 const Cart = () => {
     const { items } = getCartSlice();
+    useEffect(() => {
+        console.log(items);
+    },[items]);
     return (
         <>
             <div className="d-flex justify-content-center vh-100 bg-secondary-subtle">
-                <div className="d-flex flex-column" style={{ width: '60vw' }}>
-                    <div>
+                <div className="d-flex flex-column" style={{ width: '55vw' }}>
+                    <h3 className='mt-4'>
                         Carro de compras
-                    </div>
-                    {items.map(() => <CartItem/>)}
+                    </h3>
+                    {items.map((item, itemIndex) =>
+                        <CartItem
+                            _id={item._id}
+                            name={item.name}
+                            image={item.image}
+                            currentQuantity={item.currentQuantity}
+                            price={item.price}
+                            discount={item.discount}
+                            quantityToBuy={item.quantityToBuy}
+                            key={itemIndex}
+                        />
+                    )}
                 </div>
-                <div style={{ width: '20vw' }}>
-                    Resumen de la orden
+                <div className='d-flex flex-column' style={{ width: '20vw' }}>
+                    <h3 className='mt-4'>
+                        Resumen de la orden
+                    </h3>
+                    <div className='bg-light rounded v-100'>
+                        asd
+                    </div>
                 </div>
             </div>
         </>
