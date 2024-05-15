131fb0ddd4b1f207ff503854bd4e821bdfcaa902
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-05-15 11:13:48 -0500

    Pending countDiscounts

--- src/components/CartItems/OrderSummary.jsx
+++ src/components/CartItems/OrderSummary.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getCartSlice } from '../../context/store/store';
 
 const OrderSummary = () => {
     const { items } = getCartSlice();
-    const [discounts, setDiscounts] = useState(true);
+    // const [discounts, setDiscounts] = useState({ hasDiscounts: false, countDiscounts: 0 });
+
+    useEffect(() => {
+        // countDiscounts()
+    }, []);
+
+    // const countDiscounts = () => {
+    //     const counter = items.reduce((acc, obj) => {
+    //         if (!!obj.discount) acc++;
+    //         return acc;
+    //     }, 0);
+    //     setDiscounts({ hasDiscounts: !!counter, countDiscounts: counter })
+    //     return counter;
+    // }
     return (
         <div className='bg-light rounded v-100 mt-2 flex-column p-3'>
             <div className='mb-2'>
                 Productos ({items.length})
             </div>
             <hr />
-            {discounts
+            {true
                 && <div className='mt-2'>Descuentos ()</div>
             }
         </div>
