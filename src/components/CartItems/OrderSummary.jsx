4b5fffdedf8fa243cd41455c5a4ce3f23aa6dc65
Julian1324 <julian-ortiz98@hotmail.com>
2024-05-18 23:30:47 -0500

    Pending styles in OrderSummary

--- src/components/CartItems/OrderSummary.jsx
+++ src/components/CartItems/OrderSummary.jsx
@@ -3,28 +3,69 @@ import { getCartSlice } from '../../context/store/store';
 
 const OrderSummary = () => {
     const { items } = getCartSlice();
-    // const [discounts, setDiscounts] = useState({ hasDiscounts: false, countDiscounts: 0 });
+    const [discountsSummary, setDiscountsSummary] = useState(
+        {
+            hasDiscounts: false,
+            counter: 0,
+            totalDiscount: 0
+        }
+    );
+    const [productsSummary, setProductsSummary] = useState(
+        {
+            counter: 0,
+            totalProducts: 0
+        }
+    );
 
     useEffect(() => {
-        // countDiscounts()
-    }, []);
+        const calculateDiscounts = () => {
+            const { counter, totalDiscount } = items.reduce((acc, obj) => {
+                if (!!obj.discount) {
+                    acc = {
+                        ...acc,
+                        counter: (acc.counter + 1) || 1,
+                        totalDiscount: (acc.totalDiscount + (obj.price * obj.discount / 100))
+                            || (obj.price * obj.discount / 100)
+                    };
+                };
+                return acc;
+            }, {});
+            setDiscountsSummary((prevState) => ({
+                ...prevState,
+                hasDiscounts: !!counter,
+                counter,
+                totalDiscount
+            }))
+        }
+        const calculateProductsSummary = () => {
+            const { counter, totalProducts } = items.reduce((acc, obj) => {
+                acc = {
+                    ...acc,
+                    counter: (acc.counter + 1) || 1,
+                    totalProducts: (acc.totalProducts + obj.price) || obj.price
+                }
+                return acc;
+            }, {});
+            setProductsSummary((prevState) => ({
+                ...prevState,
+                counter,
+                totalProducts
+            }));
+        }
+        calculateProductsSummary();
+        calculateDiscounts();
+    }, [items]);
 
-    // const countDiscounts = () => {
-    //     const counter = items.reduce((acc, obj) => {
-    //         if (!!obj.discount) acc++;
-    //         return acc;
-    //     }, 0);
-    //     setDiscounts({ hasDiscounts: !!counter, countDiscounts: counter })
-    //     return counter;
-    // }
     return (
         <div className='bg-light rounded v-100 mt-2 flex-column p-3'>
             <div className='mb-2'>
-                Productos ({items.length})
+                Productos ({productsSummary.counter}) <span>{productsSummary.totalProducts}</span>
             </div>
             <hr />
-            {true
-                && <div className='mt-2'>Descuentos ()</div>
+            {discountsSummary.hasDiscounts
+                && <div className='mt-2'>
+                    Descuentos ({discountsSummary.counter}) <span>{discountsSummary.totalDiscount}</span>
+                </div>
             }
         </div>
     )
