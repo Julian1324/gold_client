fabddb49145ae342864f57d443d6e2d7d423409e
Julian1324 <48034248+Julian1324@users.noreply.github.com>
2024-07-26 17:20:45 -0500

    Creación de resumen de compra

--- src/pages/PurchaseSummary/Summary.jsx
+++ src/pages/PurchaseSummary/Summary.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { getCartSlice } from '../../context/store/store';
+
 const Summary = () => {
+    const { getItems } = getCartSlice();
+    const [ myItems, setMyItems ] = useState(getItems());
+
     return (
-        <>
-            Summary works!
-        </>
+        <Card style={{ width: '18rem' }}>
+            <Card.Header>Resumen de Compra</Card.Header>
+            <ListGroup variant="flush">
+                {myItems.map((item, index) => (
+                    <ListGroupItem key={index}>
+                        <div className="d-flex justify-content-between">
+                            <span>{item.name}</span>
+                            <span>${item.price.toFixed(2)}</span>
+                        </div>
+                    </ListGroupItem>
+                ))}
+                <ListGroupItem>
+                    <div className="d-flex justify-content-between">
+                        <strong>Total</strong>
+                        <strong>200</strong>
+                    </div>
+                </ListGroupItem>
+            </ListGroup>
+        </Card>
     );
 }
 
