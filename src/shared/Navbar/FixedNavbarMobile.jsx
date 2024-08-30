afd141957b71eaf51f65f837cd1d30cf1b014041
Julián Andrés O. <48034248+Julian1324@users.noreply.github.com>
2024-08-30 17:12:47 -0500

    Making onclick fixed navbar

--- src/shared/Navbar/FixedNavbarMobile.jsx
+++ src/shared/Navbar/FixedNavbarMobile.jsx
@@ -1,10 +1,64 @@
 const FixedNavbarMobile = () => {
+
+    const IconBox = () => {
+        return (
+            <div className={'d-flex align-items-center'}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
+            </div>
+        );
+    }
+
+    const MaterialMenu = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24"><path fill="currentColor" d="M3 18v-2h18v2zm0-5v-2h18v2zm0-5V6h18v2z" /></svg>
+        )
+    }
+
+    const repeatedStyle = "d-flex flex-column align-items-center justify-content-center";
+
+    const Home = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m2.25 12l8.955-8.955a1.124 1.124 0 0 1 1.59 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
+        )
+    }
+
+    const Cart = () => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-cart2" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+            </svg>
+        )
+    }
+
     return (
         <div
             className="bg-light fixedMenu"
-            style={{height: '10vh', width: '100dvw', zIndex: '99'}}
+            style={{ height: '9vh', width: '100dvw', zIndex: '99' }}
         >
-Fixed mobile menu
+            <div className="d-flex w-100 h-100 justify-content-around" style={{ fontSize: '0.85rem' }}>
+
+                <div className={repeatedStyle}>
+                    <Home />
+                    <span>Inicio</span>
+                </div>
+
+                <div className={repeatedStyle}>
+                    <IconBox />
+                    <span>Ingreso</span>
+                </div>
+
+                <div className={repeatedStyle}>
+                    <MaterialMenu />
+                    <span>Menú</span>
+                </div>
+
+                <div className={repeatedStyle}>
+                    <Cart />
+                    <span>$0</span>
+                </div>
+            </div>
         </div>
     )
 }
